2489f3704d84a0ff60d7f1738db517f7d415aef5
Non-Euclidean Dreamer <laura.kuttnig@gmail.com>
2025-05-29 21:42:38 +0200

    Adding Projections to 3D

--- AffineSpace.java
+++ AffineSpace.java
@@ -32,9 +32,147 @@ public class AffineSpace
 		p[0]=or.times(Rn.e(0, n)).c;
 		if(n%2==0)	p[1]=or.times(Rn.e(n/2, n)).c;
 		else		p[1]=or.times((Rn.e(n/2,n).add(Rn.e(n/2+1, n)).times(0.5/Math.cos(Math.PI/(2*n))))).c;
-	Matrix pos=new Matrix(p);
-	or.print();pos.print();//System.print.println(or.det();)
+		//	p[2]=Rn.e(3,4).c;
+		Matrix pos=new Matrix(p);
+		//or.print();pos.print();//System.print.println(or.det();)
 		return new AffineSpace(or,pos,a);
+	}
+	
+	public static AffineSpace bipyramidicE(int n, Rn a,Matrix rot)
+	{
+		double[][] m=new double[n+1][n+1],p=new double[3][n+1];
+		for(int i=0;i<n;i++)
+		{
+			for(int j=0;j<n;j++)
+			{
+				m[i][j]=2*Math.cos(Math.PI*(i-j)/n)/n;
+				/*m[n][n]+=m[i][j];
+				if(i!=j)m[n][n]+=m[i][j];*/
+			}
+		}
+		m[n][n]=1;
+		Matrix or=new Matrix(m);
+		p[0]=or.times(Rn.e(0, n+1)).c;
+		if(n%2==0)	p[1]=or.times(Rn.e(n/2, n+1)).c;
+		else		p[1]=or.times((Rn.e(n/2,n+1).add(Rn.e(n/2+1, n+1)).times(0.5/Math.cos(Math.PI/(2*n))))).c;
+		p[2]=Rn.e(n,n+1).c;
+		Matrix pos=new Matrix(p);
+		pos=rot.times(pos);
+		//or.print();pos.print();//System.print.println(or.det();)
+		return new AffineSpace(or,pos,a);
+	}
+	
+	public static AffineSpace prismicE(int n,Rn a,Matrix rot)//only for 4 implemendted not what I wantx 
+	{
+		Matrix[]u=new Matrix[n/2+1];
+		for(int i=0;i<u.length;i++)
+		{
+			u[i]=new Matrix(n,n);
+			for(int j=0;j<n;j++)
+			{
+				u[i].entry[j][(j+i)%n]+=1;
+				u[i].entry[j][(j-i+n)%n]+=1;
+			}
+		}
+		double[]factor= {0.75/2,0.25,-0.25};
+		if(n==5)factor=new double[] {0.3,.1-1/Math.sqrt(20),.1+1/Math.sqrt(20)};
+			Matrix 		or=u[0].times(factor[0]);
+			for(int i=1;i<u.length;i++)or=or.add(u[i].times(factor[i]));
+			
+		double[][]p=new double[3][n];
+		p[0]=or.times(Rn.e(0,n)).normalize().c;
+		p[1]=or.times(Rn.e(1,n).substract(Rn.e(n-1, n))).normalize().c;
+		p[2]=or.times(Rn.diag(n)).normalize().c;
+		Matrix pos=new Matrix(p);
+		pos=rot.times(pos);
+		//or.print();pos.print();//System.print.println(or.det();)
+			return new AffineSpace(or,pos,a);
+	}
+	
+	public static AffineSpace standardE(Lattice l, Rn a)
+	{
+		int n=l.n;
+		Matrix lat=new Matrix(l.base),inv=new Matrix(l.inv);
+		double[][] m=new double[n][n],p=new double[2][n];
+		for(int i=0;i<n;i++)
+		{
+			for(int j=0;j<n;j++)
+			{
+				m[i][j]=2*Math.cos(Math.PI*(i-j)/n)/n;
+			}
+		}
+		Matrix or=lat.times(new Matrix(m)).times(inv);
+		p[0]=or.times(Rn.e(0, n)).c;
+		if(n%2==0)	p[1]=or.times(Rn.e(n/2, n)).c;
+		else		p[1]=or.times((Rn.e(n/2,n).add(Rn.e(n/2+1, n)).times(0.5/Math.cos(Math.PI/(2*n))))).c;
+	Matrix pos=new Matrix(p);
+	//or.print();pos.print();//System.print.println(or.det();)
+		return new AffineSpace(or,pos,a);
+	}
+	public static AffineSpace tetrahedral(Rn a, Matrix rot)
+	{
+		Matrix or=new Matrix(new double[][] {{3,-1,-1,-1},{-1,3,-1,-1},{-1,-1,3,-1},{-1,-1,-1,3}}).times(0.25);
+		double[][]p=new double[3][4];
+		p[0]=or.times(Rn.e(0, 4)).c;
+		p[1]=or.times(new Rn(new double[] {1,3,0,0}).times(0.25*Math.sqrt(2))).c;
+		p[2]=or.times(new Rn(new double[] {0,0,1,-1}).times(0.5*Math.sqrt(1.5))).c;
+		Matrix po=(new Matrix(p));
+		po=rot.times(po);
+		
+	//	System.out.println(new Rn(po.entry[2]).dot(new Rn(po.entry[1])));
+	//	
+		return new AffineSpace(or, po,a);
+	}
+	
+	public static AffineSpace icosahedral(Rn a, Matrix rot)
+	{
+		Matrix u=new Matrix(new double[][] {{0,1,1,1,1,1},{1,0,1,-1,-1,1},{1,1,0,1,-1,-1},{1,-1,1,0,1,-1},{1,-1,-1,1,0,1},{1,1,-1,-1,1,0}}),or=Matrix.idmatrix(6).times(0.5).add(u.times(Math.sqrt(0.05)));
+
+		double[][]p=new double[3][6];
+		p[0]=or.times(new Rn(new double[] {1,1,0,0,0,0}).times(.6)).c;
+		p[1]=or.times(new Rn(new double[] {0,0,-1,0,0,1,}).times(.6)).c;
+		p[2]=or.times(new Rn(new double[] {0,0,0,1,1,0}).times(.6)).c;
+		Matrix po=(new Matrix(p));
+		po=rot.times(po);
+		
+	//	System.out.println(new Rn(po.entry[2]).dot(new Rn(po.entry[1])));
+	//	
+		return new AffineSpace(or, po,a);
+	}
+	public static AffineSpace danzer(Rn a, Matrix rot)
+	{
+		Matrix u=new Matrix(new double[][] {{1,3,-1,-1,3,1},{3,1,3,-1,-1,1},{-1,3,1,-1,-1,1},{-1,3,1,3,-1,1},{3,-1,-1,3,1,1},{-3,-3,-3,-3,-3,-5}}).times(.5),or=Matrix.idmatrix(6).times(0.5).add(u.times(Math.sqrt(0.05)));
+
+		double[][]p=new double[3][6];
+		p[0]=or.times(new Rn(new double[] {1,1,1,1,1,1}).times(.5)).c;
+		p[1]=or.times(new Rn(new double[] {1,0,-1,0,0,0,}).times(.6)).c;
+		p[2]=or.times(new Rn(new double[] {0,0,0,1,1,0}).times(.6)).c;
+		Matrix po=(new Matrix(p));
+		po=rot.times(po);
+		
+	//	System.out.println(new Rn(po.entry[2]).dot(new Rn(po.entry[1])));
+	//	
+		return new AffineSpace(or, po,a);
+	}
+	
+	public static AffineSpace dodecahedral(Rn a, Matrix rot)
+	{
+		
+		Matrix u=new Matrix(new double[][] {{0,1,1,1,0,0,0,0,0,0},{1,0,0,0,1,0,0,0,0,1},{1,0,0,0,0,1,1,0,0,0},{1,0,0,0,0,0,0,1,1,0},{0,1,0,0,0,1,0,-1,0,0},
+			{0,0,1,0,1,0,0,0,-1,0},{0,0,1,0,0,0,0,1,0,-1},{0,0,0,1,-1,0,1,0,0,0},{0,0,0,1,0,-1,0,0,0,1},{0,1,0,0,0,0,-1,0,1,0}}),
+			v=new Matrix(new double[][] {{0,0,0,0,1,1,1,1,1,1},{0,0,1,1,0,1,-1,-1,1,0},{0,1,0,1,1,0,0,1,-1,-1},{0,1,1,0,-1,-1,1,0,0,1},{1,0,1,-1,0,0,-1,0,-1,1},
+				{1,1,0,-1,0,0,1,-1,0,-1},{1,-1,0,1,-1,1,0,0,-1,0},{1,-1,1,0,0,-1,0,0,1,-1},{1,1,-1,0,-1,0,-1,1,0,0},{1,0,-1,1,1,-1,0,-1,0,0}}),or=Matrix.idmatrix(10).times(0.3).add(u.times(Math.sqrt(0.05))).add(v.times(0.1));
+	
+		double[][]p=new double[3][10];//todo
+		p[0]=or.times(new Rn(new double[] {1,1,0,0,0,0,0,0,0,0}).times(.6)).c;
+		p[1]=or.times(new Rn(new double[] {0,0,0,0,0,0,1,1,0,0}).times(.6)).c;
+		p[2]=or.times(new Rn(new double[] {0,0,0,0,0,-1,0,0,1,0}).times(.6)).c;
+		Matrix po=(new Matrix(p));
+		po=rot.times(po);
+		
+	//	System.out.println(new Rn(po.entry[2]).dot(new Rn(po.entry[1])));
+	//	
+		return new AffineSpace(or, po,a);
 	}
 	
 	public static AffineSpace subplane(int n, Rn a)
@@ -46,10 +184,20 @@ public class AffineSpace
 		Matrix pos=new Matrix(p); Matrix or=new Matrix(m);
 		return new AffineSpace(or,pos,a);
 	}
-	//embed in a higher dimension
-	public AffineSpace embed(int dim)
+	public static AffineSpace subspace(int n, int k,Rn a)
 	{
-		Matrix or=Matrix.idmatrix(dim),pos=new Matrix(d,dim);
+		double[][]m=new double[n][n],p=new double[k][n];
+		for(int i=0;i<k;i++)
+		{m[i][i]=1;
+		p[i][i]=1;}
+		Matrix pos=new Matrix(p); Matrix or=new Matrix(m);
+		return new AffineSpace(or,pos,a);
+	}
+	//embed in a higher dimension
+	public AffineSpace embed(int dim,int k)
+	{
+		Matrix or=Matrix.idmatrix(dim),
+				pos=new Matrix(k,dim);
 		Rn anew=Rn.zero(dim);
 		for(int i=0;i<n;i++)
 		{	
@@ -59,6 +207,13 @@ public class AffineSpace
 			
 			for(int j=0;j<d;j++)
 				pos.entry[j][i]=position.entry[j][i];
+		
+		}	
+		if(d<k) {Rn[]nullspace=or.nullspace();System.out.print("nullspace:");nullspace[0].print();
+		for(int j=d;j<k;j++)
+			{
+				pos.entry[j]=nullspace[j-d].normalize().c;
+			}
 		}
 		return new AffineSpace(or,pos,anew);
 	}
@@ -80,5 +235,20 @@ public class AffineSpace
 	
 		Matrix pos=matrix.times(position.transpone()).transpone();
 		return new AffineSpace(or, pos,a);
+	}
+	public static AffineSpace fullSpace(int n2,Matrix rot,Rn a) {
+		
+		return new AffineSpace(Matrix.idmatrix(n2),rot,a);
+	}
+	
+	public AffineSpace dualSpace()//pos not done
+	{
+		Matrix or=Matrix.idmatrix(n).subtract(orientation);
+		double[][]	p=new  double[n-d][n];
+		for(int i=0;i<n-d;i++)
+		{
+			//todo
+		}
+		return new AffineSpace(or,new Matrix(p),a);
 	}
 }
--- ProjectiveTiling.java
+++ ProjectiveTiling.java
@@ -8,89 +8,134 @@ import java.awt.Color;
 import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
+import java.text.DecimalFormat;
 import java.util.ArrayList;
+import java.util.Random;
+
 import javax.imageio.ImageIO;
 
 //*******************************************
 // Draws the projective Tiling for a given lattice and affine plane in Rn
 //******************************************************
 public class ProjectiveTiling 
-{
-	static int[][] colors= {{0,0,0},{255,0,0},{255,0,255},{255,255,0},{0,255,255},{128,255,128},{128,255,255},{128,128,255},{255,128,128},{0,85,0},{0,0,85},{170,255,255}};
-	static int width=2560,height=1440;
-	static double scale=10;// 
+{ 
+	
+	static DecimalFormat df=new DecimalFormat("0000");
+	static int[][] colors= {{0,255,255},{255,0,255},{255,255,128},{0,255,128},{255,128,255},{128,255,0},{0,0,0},{255,128,0},{128,0,0},{0,0,255},{72,154,0}};
+	static int width=1080,height=1080,depth=(int) (height/6*Math.sqrt(5)),st=10;
+	static double scale=6,range=1,r=width/2/Math.PI;
 	static BufferedImage canvas;
-	static int n=3, counter=0;//n=dimension, counter=index of first image 
-	 static Rn a; 
+	static double[][]zBuffer=new double[width][height];
+	static int n=6, k=3,counter=st-1;//n=dimension,k=dim. of projection, counter+1=index of first image 
+	 static Rn a,v; 
 	 static Polytope zerocell; 
 	static  Lattice lattice;
 	static  AffineSpace plane;	
-	static String name="Powerup";
-	static boolean inPicture;
+	static String name="test";
+	static boolean inPicture,chunk=false,cylinder=false;
 	 static ArrayList<int[]> archive=new ArrayList<int[]>(),archivev=new ArrayList<int[]>(),//To check whether it has been done already
 				tiles=new ArrayList<int[]>();
-		static	double[][] base=Matrix.idmatrix(n).entry;
+		static	double[][] base=Matrix.idmatrix(n).entry;//lattice.base;//
 	static	int[]start=new int[base.length],loc;//To rethink for other types of lattices!
 
 	public static void main(String[] args)
 	{ 
-		System.out.print("start");
 		lattice=new Lattice(base);
-		a=new Rn(new double[] {1,1,-1/*,-0.5,0.5,0.5,0.5,0.5,0.5,0.5,0.5,0.5*/}).times(Math.PI-3)/*.add(Rn.e(4, n).times(0.5))*/;
+	base=lattice.base;
+		System.out.print("start");
+		a=new Rn(new double[] {1,-1,1,-1,0,1}).times(0.01*Math.PI).add(Rn.diag(n).times(0.5));
+		v=new Rn(new double[] {1,1,1,1,1,1,1,1,1,1}).times(1.0/1440);
 		Matrix rot=new Matrix(new double[][] {{0,0.5,0,-0.5,0},{0.5,Math.sqrt(2)/2,0.5,Math.sqrt(2)/2,0},{Math.sqrt(2)/2,0.5,-Math.sqrt(2)/2,-0.5,0},{0.5,0,0.5,0,0},{0,0,0,0,1}});
-		rot.orthonormalize();
+
+	Random rand=new Random();
 	
-	AffineSpace plane1=AffineSpace.standardE(n-1, a).embed(n),plane2=AffineSpace.standardE(n, a);
-		rot=plane1.rotMatrix()/*.linComb(plane2.rotMatrix(),new int[] {0}, 1)*/;rot.orthonormalize();plane1=AffineSpace.subplane(n, a).transform(rot,rot.transpone());
-		rot=plane2.rotMatrix();rot.orthonormalize();plane2=AffineSpace.subplane(n, a).transform(rot, rot.transpone());
-		zerocell=Polytope.zerocell(lattice); 
-		//Video translating affine plane:
-		//for(double t=-0.0;t<1/*Math.sqrt(n)*/;t+=0.01) {a=new Rn(new double[] {1,0,0,1,0,0}).times(t).add(Rn.e(0,n).times(0.5));plane=AffineSpace.standardE(n, a);counter++;tiles.clear();archive.clear();archivev.clear();
+	
+	//for slides between two projections
+	//AffineSpace plane1=AffineSpace.icosahedral(a,Matrix.idmatrix(3)).embed(10, 3),plane2=AffineSpace.dodecahedral(a,Matrix.idmatrix(3));
+	
+
+	for(double t=0/1440.0;t<1;t+=1.0/1440)
+	{
+
+	//for slides between two projections
+	//rot=plane1.rotMatrix().linComb(plane2.rotMatrix(),new int[] {j}, new double[] {t});rot.orthonormalize();plane=AffineSpace.subspace(n,k, a).transform(rot,rot.transpone());
+	
+	
+	//zooming out
+	//	scale*=1.001;
+	//	range=range*0.99+depth*(1-1/scale)*0.01;
+
+	//Video translating affine plane: 
+	//	v=v.add(Rn.e(rand.nextInt(n),n).times((rand.nextDouble()-0.5)*.01));
+	//a=a.add(v)/*new Rn(new double[] {1,1,1,1,1}).times(t)*/;
 		
-		//Video rotating the plane
-		for(double t=1;t>0;t-=0.005) {System.out.println("t="+t);// /*Rotation along certain plane:*/plane=AffineSpace.standardE(n,a).transform(Matrix.drotate(t,1,0,2,n),Matrix.drotate(-t,1,0,2, n));
-			/*Rotating plane1 into plane2*/	 rot=plane1.rotMatrix().linComb(plane2.rotMatrix(),new int[] {0,1}, new double[] {t,t});
-				 rot.orthonormalize();
-				plane=AffineSpace.subplane(n, a).transform(rot, rot.transpone())//.transform(Matrix.hrotate(t,0,3, n),Matrix.hrotate(-t,0,3,n)).transform(Matrix.hrotate(Math.asin(1/Math.sqrt(3)), 1, 2, n), Matrix.hrotate(-Math.asin(1/Math.sqrt(3)), 1, 2, n)).transform(Matrix.hrotate(Math.PI/4,0,2,n),Matrix.hrotate(-Math.PI/4, 0, 2, n))
-				; counter++;tiles.clear();archive.clear();archivev.clear();
-		//For single image:
-		//{
+	//Rotate View
+	rot=Matrix.hrotate(t*2*Math.PI,0, 2, 3).times(Matrix.hrotate(0.1, 0, 1, 3));
+		
+	plane=AffineSpace.bipyramidicE(n-1,a,rot);
+		//AffineSpace.dodecahedral(a,rot);
+		//AffineSpace.bipyramidicE(n-1,a,rot);
+		//AffineSpace.standardE(4, a);
+		//AffineSpace.tetrahedral(a,rot);//standardE(n, a);
+	
+
+		counter++;zerocell=Polytope.zerocell(lattice); 	tiles.clear();archive.clear();archivev.clear();zBuffer=new double[width][height];
+		System.out.println(counter);
 				
 		for(int i=0;i<base.length;i++)
 		{
 			start[i]=(int)Math.round(a.get(i));
 		}
-		System.out.print("{");
+	
 		canvas=new BufferedImage(width,height, BufferedImage.TYPE_3BYTE_BGR);
 		
-		int k=0;
-
 		//start looking for tiles at staring point
 		findTiles(start);	archivev.add(start);
 		while(!tiles.isEmpty()) 
 		{
-			//System.out.print("set up... ");
-			loc=tiles.get(0);
-			if(doesntContain(archive,loc))
+		//	System.out.print("set up... ");
+			loc=tiles.get(0); 
+			Rn d=Rn.zero(n);
+			for(int i=0;i<k;i++)d=d.add(Rn.e(loc[n+i], n));
+		if(chunk)	inPicture=plane.orientation.times(a.substract(lattice.vertex(loc).add(new Matrix(lattice.base).times(d.times(0.5))))).norm()<scale/3;
+			int[]st=loc;
+			int q=1;
+			if(k==3)q=6;
+			for(int i=0;i<q;i++) {
+				if(k==3) {
+					st=new int[n+2];
+					for(int j1=0;j1<n;j1++)st[j1]=loc[j1];
+					if(i>2)st[loc[n+i-3]]++;
+					int r=0;
+					for(int j1=0;j1<3;j1++)if(j1!=i%3) {
+						st[n+r]=loc[n+j1];
+						r++;
+					}
+				}
+			if(doesntContain(archive,st))
 			{
-				//System.out.println("draw...");
-				
-				drawTile(loc);
-				//System.out.println("and archive");
-				archive.add(loc);
-				//print(canvas,k);k++;
-			}
+			//	System.out.println("draw...");
+			//	print(st);System.out.println();
+				drawTile(st);
+			//	System.out.println("and archive");
+				archive.add(st);
+		//		print(canvas,counter);counter++;
+			}}
 			tiles.remove(0);
 			
 		}
-		System.out.println("},");System.out.println();
-		print(canvas,counter);
+	//	System.out.println("},");System.out.println();
+		print(canvas,counter);//counter++;
+		
 	}
+//	rot=plane1.rotMatrix().linComb(plane2.rotMatrix(),new int[] {j}, new double[] {1});rot.orthonormalize();plane1=AffineSpace.subspace(n,k, a).transform(rot,rot.transpone());
+
+	
 	}
 	private static boolean doesntContain(ArrayList<int[]>archive,int[] array)
 	{
 		int n=array.length;
-		boolean same;
+		boolean same; 
 		for(int[] x : archive)
 		{
 			same=true;
@@ -108,7 +153,7 @@ public class ProjectiveTiling
 	
 	//returns whether the tile is in the picture at all
 	public static void drawTile(int[] tile)
-	{ boolean inPicture;
+	{ 
 		int[][]corner=new int[4][n];
 		for(int i=0;i<n;i++)
 		{
@@ -117,6 +162,9 @@ public class ProjectiveTiling
 				corner[j][i]=tile[i];
 			}
 		}
+		/*int color=0,cl=0;
+		while(color!=tile[n]&&color!=tile[n+1]) {color++;cl+=n-color;}color++;
+		while(color!=tile[n]&&color!=tile[n+1]) {color++;cl++;}*/
 		//System.out.print("define corners...");
 		corner[1][tile[n]]+=1;
 		corner[2][tile[n]]+=1;
@@ -127,112 +175,137 @@ public class ProjectiveTiling
 		for(int i=0;i<3;i++) //Normal project to E identified as R2--> "to lattice"
 			cs[i]=plane.position.times(plane.orientation.times(lattice.vertex(corner[i])));
 	
-		int color=mix(colors[tile[n]],colors[tile[n+1]]);
+		int[] color=mix(colors[tile[n]],colors[tile[n+1]]);//new Color(colors[color][0],colors[color][1],colors[color][2]).getRGB();//
 		//System.out.print("draw..."); 
-		inPicture= draw(cs[0],cs[1],cs[2],color);
+		if(!chunk)inPicture= draw(cs[0],cs[1],cs[2],color);	
+	//	print(tile);
+	//	System.out.println(inPicture);
 		if(inPicture) {/*System.out.print("find Tiles...");*/for (int i=0;i<4;i++)
-			{findTiles(corner[i]); if(doesntContain(archivev,corner[i])&&i<3) {archivev.add(corner[i]);cs[i].print();}}
-		}
+			{if(doesntContain(archivev,corner[i])) {findTiles(corner[i]); archivev.add(corner[i]);	/*cs[i].print();*/}}
+	//print(tile);	print(corner[0]);print(corner[1]);print(corner[2]);System.out.println();	print(canvas,counter);counter++;
+	if(chunk)draw(cs[0],cs[1],cs[2],color);	}
 	} 
 	
 	private static void findTiles(int[] start) 
 	{
+		int c=tiles.size();
 		//Polytope cell=zerocell.shift(lattice.vertex(start));
-
-		for(int i=0;i<n;i++)
-				{ 
-					for(int j=i+1;j<n;j++) 
-					{
-						Polytope window=lattice.dual2cell(start,i,j,plane);
+		int[]index=new int[k];
+		for(int a=1;a<k;a++)index[a]=a;
+		index[k-1]=k-2;
+		while(nextIndex(index)) 
+		{
+			int i=index[0],j=index[1],h;
+				for(int s=0;s<8;s++)	
+				{
+					int[]star=start.clone();
+					for(int b=0;b<n;b++)star[b]=start[b];
+					for(int b=0;b<k;b++)if(s/(int)Math.pow(2, b)%2==1)star[index[b]]--;
+						Polytope window=lattice.dual2cell(star,i,j,plane);
+						if(k==3) 
+						{
+							h=index[2];
+							window=lattice.dual3cell(star,i,j,h,plane);
+						}
+						
 						boolean contains=a.isIn(window);
 						if(contains==true)
 						{ //System.out.print("in!"); 
-								int[] entry=new int[n+2]; 
-								for(int k=0;k<n;k++)
+								int[] entry=new int[n+k]; 
+								for(int k1=0;k1<n;k1++)
 								{
-									entry[k]=start[k];
+									entry[k1]=star[k1];
 								}
-								entry[n]=i; 
-								entry[n+1]=j;
+								for(int k1=0;k1<k;k1++)
+								entry[n+k1]=index[k1]; 
 								tiles.add(entry);
-							}
-						start[i]-=1;
-						window=lattice.dual2cell(start,i,j,plane);
+						//		print(entry);
+							//	print(entry);System.out.println();
+						}
+					//	else print(star);System.out.print(s+", "+index[0]+", "+index[1]+", "+index[2]+", ");window.print();
 						
-						if(a.isIn(window))
-							{  // System.out.print("in!");
-								int[] entry=new int[n+2];
-								for(int k=0;k<n;k++)
-								{
-									entry[k]=start[k];
-								}
-								entry[n]=i;
-								entry[n+1]=j;
-								tiles.add(entry);
-							}
-						start[j]-=1;
-						window=lattice.dual2cell(start,i,j,plane);
-						
-						if(a.isIn(window))
-							{//System.out.print("in!");
-								int[] entry=new int[n+2];
-								for(int k=0;k<n;k++)
-								{
-									entry[k]=start[k];
-								}
-								entry[n]=i;
-								entry[n+1]=j;
-								tiles.add(entry);
-							}
-						start[i]+=1;
- window=lattice.dual2cell(start,i,j,plane);
-						
-						if(a.isIn(window))
-							{  //System.out.print("in!");
-								int[] entry=new int[n+2];
-								for(int k=0;k<n;k++)
-								{
-									entry[k]=start[k];
-								}
-								entry[n]=i;
-								entry[n+1]=j;
-								tiles.add(entry);
-							}
-						start[j]+=1;
-					}
 				}
+		}
+	//	System.out.print( "For loc=");print(start);System.out.println(" we have"+(tiles.size()-c)+" candidates");
 	}
 	
+	private static boolean nextIndex(int[] index) {
+		int c=k-1;
+		while(c>-1&&index[c]==n-k+c)c--;
+		if(c<0)return false;
+		
+		index[c]++;
+		for(int a=c+1;a<k;a++)index[a]=index[c]+a-c;
+		return true;
+	}
 	//Draw the tile with given vertices...
-	public static boolean draw(Rn x, Rn b, Rn c, int color)
+	public static boolean draw(Rn x, Rn b, Rn c, int[] color)
 	{
-		Rn s=plane.position.times(a), middle=new Rn(new double[] {width/2,height/2});
+		Rn s=plane.position.times(plane.orientation.times(a)), middle=new Rn(new double[] {width/2,height/2,range});
+		if(cylinder) {
+			middle.c[0]=0;
+			middle.c[2]=r*(scale+1)/scale;
+		}
 		double[] xc=(x.substract(s).times(width/scale).add(middle)).c,
 				 bc=(b.substract(s).times(width/scale).add(middle)).c,
-				cc=(c.substract(s).times(width/scale).add(middle)).c;//new Rn(bc).print();
+				cc=(c.substract(s).times(width/scale).add(middle)).c;
+	
 		return drawRhomb(bc,xc,cc,color);
 	}
-	public static boolean drawRhomb(double[] u, double[]v, double[]w, int color)
+	private static double[] toCylinder(double[] v) {
+		double z=Math.sqrt(v[0]*v[0]+v[2]*v[2]),x=Math.atan2(v[2], v[0]);
+		return new double[] {(x/Math.PI+1)*width/2,v[1],r+depth-z};
+	}
+	public static boolean drawRhomb(double[] u, double[]v, double[]w, int[] color)
 	{
-		boolean out=false;
-		int[] u0=Int(u),v0=Int(v), w0=Int(w);
-		int x,y;
-		int vl=1+(int)Math.sqrt(Math.pow(v0[0]-u0[0], 2)+Math.pow(v0[1]-u0[1], 2)),
-				wl=1+(int)Math.sqrt(Math.pow(w0[0]-u0[0], 2)+Math.pow(w0[1]-u0[1], 2));
+		boolean out=false,dim=(u.length>2);
+	
+		int[] u0=Int(u),v0=Int(v), w0=Int(w),x=new int[u.length];//print(u0);print(v0);print(w0);
+		double[]y=new double[u.length];
+		double vl=Math.abs(v[0]-u[0])+Math.abs(v[1]-u[1]),//2+(int)Math.sqrt(Math.pow(v0[0]-u0[0], 2)+Math.pow(v0[1]-u0[1], 2)),
+				wl=Math.abs(w[0]-u[0])+Math.abs(w[1]-u[1]);//=2+(int)Math.sqrt(Math.pow(w0[0]-u0[0], 2)+Math.pow(w0[1]-u0[1], 2));
+	if(cylinder) {vl+=Math.abs(v[2]-u[2]);wl+=Math.abs(w[2]-u[2]);vl*=2;wl*=2;}
+		int c=new Color(color[0],color[1],color[2]).getRGB();
 		for(int i=0;i<vl;i++)
 		{
 			for(int j=0;j<wl;j++)
 			{
-				x=u0[0]+i*(v0[0]-u0[0])/vl+j*(w0[0]-u0[0])/wl;
-				y=u0[1]+i*(v0[1]-u0[1])/vl+j*(w0[1]-u0[1])/wl;
+				for(int k=0;k<u.length;k++)
+				x[k]=(int)(u0[k]+i*(v0[k]-u0[k])/vl+j*(w0[k]-u0[k])/wl);
 				
-				if(x>-1 && x<width&&y>-1&&y<height)
+				if(cylinder)
 				{
-					out=true;
-					canvas.setRGB(x,y,color);
+					for(int k=0;k<u.length;k++)
+						y[k]=(int)(u0[k]+i*(v0[k]-u0[k])/vl+j*(w0[k]-u0[k])/wl);
+						
+					x=toInt(toCylinder(y));
+				}
+				
+				if(x[0]>-1 && x[0]<width&&x[1]>-1&&x[1]<height&&(!dim||(x[2]>-1&&x[2]<depth)))//&&(!dim||(x[2]>-depth&&x[2]<zBuffer[x[0]][x[1]]+depth)))
+				{
+					if(dim){if(x[2]>zBuffer[x[0]][x[1]]) {
+						double factor=1.0*x[2]/depth;
+						out=true;
+						//System.out.println(factor);
+						c=new Color((int)(color[0]*factor),(int)(color[1]*factor),(int)(color[2]*factor)).getRGB();
+					zBuffer[x[0]][x[1]]=x[2];
+					
+					canvas.setRGB(x[0],x[1],c);}}
+					else
+					{
+						out=true;
+					
+						canvas.setRGB(x[0],x[1],c);
+					}
+						
 				}
 			}
 		}
+		return out;
+	}
+	private static int[] toInt(double[] v) {
+		int[] out= new int[v.length];
+		for(int i=0;i<v.length;i++) out[i]=(int)v[i];
 		return out;
 	}
 	public static int[] Int(double[] u)
@@ -245,18 +318,18 @@ public class ProjectiveTiling
 		return out;
 	} 
 	//take the r,g,b of two colors, give the rgb of their average
-	public static int mix(int[] c1, int[]c2)
+	public static int[] mix(int[] c1, int[]c2)
 	{
 		Color col1=new Color(c1[0],c1[1],c1[2]),col2=new Color(c2[0],c2[1],c2[2]);
 		
-		return (new Color((col1.getRed()+col2.getRed())/2,(col1.getGreen()+col2.getGreen())/2,(col1.getBlue()+col2.getBlue())/2 )).getRGB();
+		return (new int[] {(col1.getRed()+col2.getRed())/2,(col1.getGreen()+col2.getGreen())/2,(col1.getBlue()+col2.getBlue())/2 });
 	}
 	public static  void print(BufferedImage canvas, int k)
 	{
-		File outputfile = new File(name+k+".jpg");
+		File outputfile = new File(name+df.format(k)+".png");
 		try 
 		{
-			ImageIO.write(canvas, "jpg", outputfile);
+			ImageIO.write(canvas, "png", outputfile);
 		} catch (IOException e) {
 			System.out.println("IOException");
 			e.printStackTrace(); 
